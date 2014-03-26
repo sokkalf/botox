e16d6b73506f6c1174a690ac4bf124931b4891ce
Christian Lønaas <christian.lonaas@discombobulator.org>
2014-03-26 19:46:56 +0100

    Keep file handle open, avoiding re-opening of logfile on every line

--- plugins/log_file.rb
+++ plugins/log_file.rb
@@ -1,13 +1,21 @@
 class Plugins
+  def open_logfile(channel)
+    Plugins.open_logfile[channel] ||= File.open("#{channel}.log", "a")
+  end
+
+  def self.open_logfile
+    @logfiles ||= Hash.new
+  end
+
   def log_message_to_file
     log_line = yield
     timestamp = log_line['timestamp']
     nick = log_line['mask']
     channel = log_line['channel']
     message = log_line['message']
-    f = File.open("#{channel}.log", "a")
+    f = open_logfile(channel) #File.open("#{channel}.log", "a")
     f.puts "#{timestamp}: <#{get_nick_from_prefix(nick)}> #{message}"
-    f.close
+    f.flush
   end
 end
 
