1547c54ed50742fddfd6a83b974c0b001801dbec
Christian Lønaas <christian.lonaas@discombobulator.org>
2014-03-26 19:50:33 +0100

    Keep elasticsearch connection open

--- plugins/log_elasticsearch.rb
+++ plugins/log_elasticsearch.rb
@@ -2,10 +2,18 @@ require 'elasticsearch'
 require 'json'
 
 class Plugins
+  def get_elasticsearch_client
+    Plugins.get_elasticsearch_client
+  end
+
+  def self.get_elasticsearch_client
+    @client ||= Elasticsearch::Client.new log: false
+  end
+
   def log_message_to_elasticsearch
     log_line = yield
-    client = Elasticsearch::Client.new log: false
-    response = client.index index: 'irc_log', type: 'irc_message', body: log_line.to_json
+    #client = Elasticsearch::Client.new log: false
+    response = get_elasticsearch_client.index index: 'irc_log', type: 'irc_message', body: log_line.to_json
   end
 end
 
