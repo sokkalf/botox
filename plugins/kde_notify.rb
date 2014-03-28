5b31b1ad1e6c4ecbfc86bd1c86d39ee9b8997a61
Christian Lønaas <christian.lonaas@discombobulator.org>
2014-03-28 22:18:54 +0100

    Support multiple highlight words

--- plugins/kde_notify.rb
+++ plugins/kde_notify.rb
@@ -5,7 +5,7 @@ class Plugins
     nick = get_nick_from_prefix(log_line['mask'])
     channel = log_line['channel']
     message = log_line['message']
-    %x{kdialog --title 'IRC: #{channel}' --passivepopup '#{nick}: #{message}' 15} if message =~ /#{get_config['highlight']}/i
+    %x{kdialog --title 'IRC: #{channel}' --passivepopup '#{nick}: #{message}' 15} if message.upcase =~ Regexp.union(*get_config['highlight'].map{|h| h.upcase})
   end
 end
 
